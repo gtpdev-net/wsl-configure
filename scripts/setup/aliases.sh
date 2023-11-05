d0ad487194df5321883f4075e02c3d125095a224
Geoffrey Prest <geoffrey.prest@gtpdev.net>
2023-11-04 20:28:57 -0400

    Adds aliases

---
+++ scripts/setup/aliases.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+alias toolbox='/opt/jetbrains-toolbox/jetbrains-toolbox'
+alias intellij='sh ~/.local/share/JetBrains/Toolbox/apps/intellij-idea-ultimate/bin/idea.sh > /dev/null 2>&1 &'
